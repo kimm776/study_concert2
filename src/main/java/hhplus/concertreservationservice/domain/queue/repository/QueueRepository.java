2546a436ddf1e7293c878af77d94d4f65baefa1b
kimm776 <88071886+kimm776@users.noreply.github.com>
2024-11-08 02:32:19 +0900

    대기열 구현

--- src/main/java/hhplus/concertreservationservice/domain/queue/repository/QueueRepository.java
+++ src/main/java/hhplus/concertreservationservice/domain/queue/repository/QueueRepository.java
@@ -5,23 +5,34 @@ import hhplus.concertreservationservice.domain.queue.entity.QueueStatusType;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+
 import org.springframework.data.domain.Pageable;
 
 public interface QueueRepository {
+//
+//    Queue save(Queue queue);
+//
+//    Optional<Queue> findByUserId(Long userId);
+//
+//    Optional<Queue> findByQueueToken(String token);
+//
+//    void delete(Queue queue);
+//
+//    long countWaitingUsersBefore(Long id);
+//
+//    List<Queue> findByStatusOrderByIdAsc(QueueStatusType queueStatusType, Pageable pageable);
+//
+//    List<Queue> findExpiredQueues(QueueStatusType queueStatusType, LocalDateTime now);
+//
+//    void deleteAllInBatch(List<Queue> expiredQueues);
 
-    Queue save(Queue queue);
 
-    Optional<Queue> findByUserId(Long userId);
+    void addToQueue(String token, double currentTimeMillis);
+    Long getQueuePosition(String userId);
+    Set<String> getWaitQueueData(int start, int end);
+    void setActiveStatus(String userId);
+    void removeWaitQueue(String userId);
+    void deleteActiveToken(String userId);
 
-    Optional<Queue> findByQueueToken(String token);
-
-    void delete(Queue queue);
-
-    long countWaitingUsersBefore(Long id);
-
-    List<Queue> findByStatusOrderByIdAsc(QueueStatusType queueStatusType, Pageable pageable);
-
-    List<Queue> findExpiredQueues(QueueStatusType queueStatusType, LocalDateTime now);
-
-    void deleteAllInBatch(List<Queue> expiredQueues);
 }
